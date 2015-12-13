8c86fe61661de1edb2ad7e3f90222dfc67e87ea0
Robin Gower <robin.git@infonomics.ltd.uk>
2015-12-13 17:20:06 +0100

    Streamline powerline symbols script

--- install-powerline-symbols.sh
+++ install-powerline-symbols.sh
@@ -1,6 +1,5 @@
-mkdir -p ~/.fonts
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -O ~/.fonts/PowerlineSymbols.otf
-fc-cache -vf ~/.fonts/
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -O /tmp/10-powerline-symbols.conf
+mkdir -p ~/.local/share/fonts
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -O ~/.local/share/fonts/PowerlineSymbols.otf
+fc-cache -v -f
 mkdir -p ~/.config/fontconfig/conf.d/
-mv /tmp/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -O ~/.config/fontconfig/conf.d/10-powerline-symbols.conf
